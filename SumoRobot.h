8f28acc5683fb67e3539764f84c49b0cafcb5e07
NoName64Bits <ciobanulaur@gmail.com>
2015-06-07 14:15:12 +0300

    added comments at the begining of the header file

--- SumoRobot.h
+++ SumoRobot.h
@@ -1,3 +1,12 @@
+// This is the header file for the Sumo Robot Library for Arduino 
+// by #NoName and #NoFear (Ciobanu Laurentiu and Tenea Mihai)
+// If you have any problems please contact us at:
+// aplharoboticsteam@gmail.com or at ciobanulaur@gmail.com
+// Syntax of library is shown and described in the software 
+// examples that are attached to this library
+// Good luck with your sumo robot!
+
+
 #ifndef SumoRobot_h
 #define SumoRobot_h
 
